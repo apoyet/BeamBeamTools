3d0bdd815d552eb3ea58583ddec53d3f4cb8c5fd
apoyet <32321729+apoyet@users.noreply.github.com>
2018-04-25 11:44:49 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='BeamBeamTools',
+      version='0.01',
+      description='TODO',
+      url='https://github.com/apoyet/BeamBeamTools',
+      author='Axel POYET',
+      author_email='axel.poyet@cern.ch',
+      license='MIT',
+      packages=['BeamBeamTools'],
+zip_safe=False)
