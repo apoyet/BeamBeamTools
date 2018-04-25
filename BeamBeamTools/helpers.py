e6542533b2c3e8cc861f449d110eebbe86b9c024
apoyet <32321729+apoyet@users.noreply.github.com>
2018-04-25 15:39:08 +0200

    Update helpers.py
    Adding utilityFunctions class, containing for the moment the setArrowLabel function.

--- BeamBeamTools/helpers.py
+++ BeamBeamTools/helpers.py
@@ -41,3 +41,22 @@ class dotdict(dict):
     def __setstate__(self, state):
         self.update(state)
         self.__dict__ = self
+        
+        
+class utilityFunctions:
+    '''
+    This class contains several usuful functions. 
+    '''
+    def setArrowLabel(ax, label='myLabel',arrowPosition=(0,0),labelPosition=(0,0), myColor='k', arrowArc_rad=-0.2):
+        '''
+        Credits to G. Sterbini.
+        '''
+        return ax.annotate(label,
+                      xy=arrowPosition, xycoords='data',
+                      xytext=labelPosition, textcoords='data',
+                      size=10, color=myColor,va="center", ha="center",
+                      bbox=dict(boxstyle="round4", fc="w",color=myColor,lw=2),
+                      arrowprops=dict(arrowstyle="-|>",
+                                      connectionstyle="arc3,rad="+str(arrowArc_rad),
+                                      fc="w", color=myColor,lw=2), 
+                      )
